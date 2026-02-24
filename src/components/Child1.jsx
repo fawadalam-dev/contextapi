98277697f00ae361891865c580d19f608aeb895a
Fawad Alam <fawadalam5813@gmail.com>
2026-02-24 14:38:05 +0500

    upload

---
+++ src/components/Child1.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Child2 from "./Child2";
+const Child1 = () => {
+    return (
+        <div>This is Child1 
+            <Child2 />
+        </div>
+    )
+}
+export default Child1;
---
+++ src/components/Child3.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+
+const Child3 = () => {
+    return (
+        <div>This is Child3 </div>
+    )
+}
+export default Child3;
